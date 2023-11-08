03dece799b6566b93141e56446e38d641fc80cb3
hanoilulu <hanoilulu@gmail.com>
2023-11-08 13:33:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useState, useSyncExternalStore } from "react";
 import './App.css';
 
 function App() {
+  const [tasks, settasks] = useState([
+    {title: "Learn JS", status: 0},
+    {title: "Code a Todo List", status: 0},
+  ])
+  const [showIncomplete, setshowIncomplete] = useState(false);
+  const [newTask, setnewTask] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">
+        Todo List
+        <span>Get things done, one item at a time.</span>
+      </h1>
+      <ul className="task-list">
+        <li>
+          <span className="label">Learn JS</span>
+          <div className="actions">
+            <input type="checkbox" className="btn-action btn-action-done" />
+            <button className="btn-action btn-action-delete">✖</button>
+          </div>
+        </li>
+        <li>
+          <span className="label">Code a Todo List</span>
+          <div className="actions">
+            <input type="checkbox" className="btn-action btn-action-done" />
+            <button className="btn-action btn-action-delete">✖</button>
+          </div>
+        </li>
+      </ul>
+      <div className="filter-wrapper">
+        <label htmlFor="filter" className="filter-label">
+          Show incompleted tasks only
+        </label>
+        <input type="checkbox" id="filter" />
+      </div>
+      <form action="#" className="form">
+        <label htmlFor="newitem">Add to the todo list</label>
+        <input type="text" id="newitem" />
+        <button type="submit">Add Item</button>
+      </form>
     </div>
   );
 }
