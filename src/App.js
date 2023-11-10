d749ea19b7f94cee51d12b5c8a788ca66c6abb3f
hanoilulu <hanoilulu@gmail.com>
2023-11-10 14:07:35 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,65 +1,54 @@
-import React, { useState, useSyncExternalStore } from "react";
+import React, { useState } from "react";
+import Header from "./components/Header";
 import './App.css';
+import TaskList from "./components/TaskList";
+import AddTaskForm from "./components/AddTaskForm";
 
 function App() {
   const [tasks, setTasks] = useState([
     { id: "task_1", title: "Learn JS Fundamentals", status: 0 },
-    { id: "task_2", title: "Code a Todo List", status: 1 },
+    { id: "task_2", title: "Code a Todo List", status: 0 },
   ])
   const [showIncomplete, setShowIncomplete] = useState(false);
   const [newTask, setNewTask] = useState("");
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  if (newTask) {
-    const task = {
-      id: Date.now(),
-      title: newTask,
-      status: 0,
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTask) {
+      const task = {
+        id: Date.now(),
+        title: newTask,
+        status: 0,
+      }
+      setTasks([...tasks, task]);
+      setNewTask("");
     }
-    setTasks([...tasks, task]);
-    setNewTask("");
   }
-}
 
-const handleInputChange = (e) => {
-  setNewTask(e.target.value);
-}
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  }
 
-const setTaskStatus = (taskId, status) => {
-  setTasks();
-}
+  const setTaskStatus = (taskId, status) => {
+    setTasks(tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, status: status ? 1 : 0 };
+      }
+      return task;
+    }));
+  }
+
+  const removeTask = (taskId) => {
+    setTasks(tasks.filter(task => task.id !== taskId));
+  }
 
   return (
     <div className="container">
-      <h1 className="title">
-        Todo List
-        <span>Get things done, one item at a time.</span>
-      </h1>
-      <ul className="task-list">
-        {tasks
-          .filter(task => showIncomplete ? task.status !== 1 : true)
-          .map((task) => (
-            <li key={task.id} className={task.status ? "done" : ""}>
-              <span className="label">{task.title}</span>
-              <div className="actions">
-                <input type="checkbox" className="btn-action btn-action-done" checked={Boolean(task.status)} onChange={(e) => setTaskStatus(task.id, e.target.checked)}>
-                <button className="btn-action btn-action-delete">✖</button>
-              </div>
-            </li>
-          ))}
-      </ul>
-      <div className="filter-wrapper">
-        <label htmlFor="filter" className="filter-label">
-          Show incompleted tasks only
-        </label>
-        <input type="checkbox" id="filter" checked={showIncomplete} onChange={(e) => setShowIncomplete(e.target.checked)} />
-      </div>
-      <form onSubmit={handleSubmit} className="form">
-        <label htmlFor="newitem">Add to the todo list</label>
-        <input type="text" id="newitem" value={newTask} onChange={handleInputChange} />
-        <button type="submit">Add Item</button>
-      </form>
+      <Header title="Todo List" subTitle="Get things done, one item at a time." />
+
+      <TaskList tasks={tasks} showIncomplete={showIncomplete} setTaskStatus={setTaskStatus} removeTask={removeTask} setShowIncomplete={setShowIncomplete} />
+
+      <AddTaskForm newTask={newTask} handleSubmit={handleSubmit} handleInputChange={handleInputChange} />
     </div>
   );
 }
